ec5e87adbc3e872f5c1a6cb191ccee034f43cd34
a-real-human <19268@wc.school.nz>
2022-04-12 14:21:52 +1200

    i did some stuff
    made ui
    made map bigger made score

---
+++ torch gods favour/Assets/scripts/score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float NumberOfTorchesLeft = 50;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
