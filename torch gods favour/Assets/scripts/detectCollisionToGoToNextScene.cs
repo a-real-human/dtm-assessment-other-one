7e3f00b0508efabccc378d653a42d59f95e6ab86
a-real-human <19268@wc.school.nz>
2022-05-05 10:10:32 +1200

    added start menu

---
+++ torch gods favour/Assets/scripts/detectCollisionToGoToNextScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectCollisionToGoToNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // to make collisions detectable and to go to next level/scene
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+}
