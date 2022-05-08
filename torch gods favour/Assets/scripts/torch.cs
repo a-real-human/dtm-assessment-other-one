7fb1c929810976518d4bebca3601e59ff962eb55
a-real-human <19268@wc.school.nz>
2022-05-09 11:18:40 +1200

    did a bit of touch up for the game

--- torch gods favour/Assets/scripts/torch.cs
+++ torch gods favour/Assets/scripts/torch.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestoryOnCollision : MonoBehaviour
+public class torch : MonoBehaviour
 
 {
+    //this allows this script to "talk to" another script
     private GameManager gameManager;
     void Start()
     {
+        //this "talks to" a specific component
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        gameManager.UpdateScore(1);
+       //destorys torch and increases the score
         Destroy(gameObject);
-
+             gameManager.UpdateScore(1);
     }
 }
