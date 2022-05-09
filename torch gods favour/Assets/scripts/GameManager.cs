5ec11c610ab5953db68c1640a9b235fa8e4e9b83
a-real-human <19268@wc.school.nz>
2022-05-09 15:26:49 +1200

    Revert "Revert "did a bit of touch up for the game""
    This reverts commit 1335369c89c71495f532d144c8563b842c33338f.

--- torch gods favour/Assets/scripts/GameManager.cs
+++ torch gods favour/Assets/scripts/GameManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
+    //score
     private int score;
+    //to display the score
     public TextMeshProUGUI scoreText;
     public List<GameObject> torches;
     // Start is called before the first frame update
@@ -16,6 +18,8 @@ public class GameManager : MonoBehaviour
 
     public void UpdateScore(int scoreToAdd)
     {
+        //increases the score
+        score += scoreToAdd;
         scoreText.text = "Score: " + score;
     }
 }
---
+++ torch gods favour/Assets/scripts/victory.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class victory : MonoBehaviour
+{
+ 
+    // to make collisions detectable and to go to next level/scene
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+     //what scene to load 
+    SceneManager.LoadScene("win scene", LoadSceneMode.Single);
+  
+    }
+}
